10403cd0d2bf69bd1d11244b93991689d95cf802
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-15 08:51:59 -0800

    adds register endpoint with add and findById in the users model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,28 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-router.post('/register', (req, res) => {
-  // implement registration
+const Users = require('../users/users-model.js');
+
+router.post('/register', async (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  try {
+    const savedUser = await Users.add(user);
+    res.status(201).json(savedUser);
+  } catch (error) {
+    if (error.code === 'SQLITE_CONSTRAINT') {
+      res.status(401).json({
+        message: `An account with that username already exists`,
+        error,
+      });
+    } else {
+      res.status(500).json(error);
+    }
+  }
 });
 
 router.post('/login', (req, res) => {
